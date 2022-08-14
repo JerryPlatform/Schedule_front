3272039559e6bfdb04e62ef950cee1351f8e419b
juseongmin <tjdals9938@naver.com>
2022-08-14 21:48:59 +0900

    [feat]: userCalendar page add .

---
+++ src/pages/calendar/userCalendar.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "userCalendar"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,15 @@
 import { RouteRecordRaw } from 'vue-router';
 
+import userCalendar from 'pages/calendar/userCalendar.vue';
+
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: "userCalendar", component: userCalendar, name: "userCalendar", props: true, }
+    ],
   },
 
   // Always leave this as last one,
