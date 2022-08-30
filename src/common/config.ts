a5a1c3c11b7e0dde12149824d6ed44fc33ce4d6d
ju <tjdals9938@naver.com>
2022-08-30 16:08:13 +0900

    [fix]: config.ts 노트북 정보 추가

--- src/common/config.ts
+++ src/common/config.ts
@@ -8,15 +8,19 @@ interface DevServer {
 }
 
 const devServer: DevServer = {
-  JSM: {
+  JSM1: {
     ip: '192.168.20.87',
+    port: 8084
+  },
+  JSM2: {
+    ip: '192.168.20.95',
     port: 8084
   }
 };
 
-const useServer: string = "JSM";
+const useServer: string = "";
 
 export const API_URL =
   useServer !== ''
   ? `http://${devServer[useServer].ip}:${devServer[useServer].port}/`
-  : process.env.API
+  : `http://192.168.20.95:8084/`
