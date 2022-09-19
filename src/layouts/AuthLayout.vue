cf12a4586fa4a255b29ba1b9007bca63174a2e55
Seong Min Ju <smju@wkit.co.kr>
2022-09-19 18:01:05 +0900

    [fix]: 로그인 레이아웃 90% 완성 처리

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -2,7 +2,11 @@
   <q-layout class="items-center flex bg-grey-1">
     <q-page-container class="container q-mx-auto">
       <div class="left">
-        <q-img src="~assets/auth_background.jpg" class="img"></q-img>
+        <div class="left-img">
+          <q-img src="~assets/auth-login-background1.png" class="sub-img" />
+          <q-img src="~assets/auth-login-background2.png" class="sub-img" />
+          <q-img src="~assets/auth-login-background3.png" class="sub-img" />
+        </div>
       </div>
       <div class="right">
         <router-view />
@@ -14,21 +18,38 @@
   </q-layout>
 </template>
 
-<!--
- <q-img src="~assets/auth_background.jpg" class="img"></q-img>
-    <router-view />
- -->
-
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import {defineComponent, onBeforeMount, ref} from 'vue'
 
 export default defineComponent ({
-  name: "AuthLayout"
+  name: "AuthLayout",
+
+  setup() {
+    let imageIndex = 0;
+    const imageClass = document.getElementsByClassName("sub-img") as HTMLCollectionOf<HTMLElement>;
+    slideShow();
+
+    function slideShow() {
+      let i = 0;
+      if (imageClass.length > 0) {
+        for (i = 0; i < imageClass.length; i++) {
+          imageClass[i].style.display = "none";
+        }
+        imageIndex++;
+        if (imageIndex > imageClass.length - 1) {
+          imageIndex = 0;
+        }
+        imageClass[imageIndex].style.display = "block";
+      }
+      setTimeout(slideShow, 3000);
+    }
+  },
 })
 </script>
+
 <style>
 .container {
-  width: 800px;
+  width: 900px;
   height: 600px;
 }
 .left {
@@ -43,8 +64,17 @@ export default defineComponent ({
   float: right;
   box-sizing: border-box;
 }
-.img {
-  width: 100%;
+.left-img {
   height: 100%;
+  padding-top: 28px;
+  background-image: url("src/assets/phone.png");
+}
+.sub-img {
+  background-color: black;
+  display: none;
+  margin-left: 150px;
+  width: 260px;
+  height: 540px;
+  border-radius: 5%;
 }
 </style>
